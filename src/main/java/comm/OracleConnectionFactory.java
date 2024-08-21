b6c33f8dee4d720f7c652da95bab708bfcbdf5b8
vsmramamurthy <vsm.ramamurthy@gmail.com>
2024-08-21 10:41:46 +0530

    Update OracleConnectionFactory.java

--- src/main/java/comm/OracleConnectionFactory.java
+++ src/main/java/comm/OracleConnectionFactory.java
@@ -1,27 +1,38 @@
 package com.example.config;
 
+import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import oracle.jdbc.pool.OracleDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
 import java.sql.SQLException;
 
 @Configuration
 public class OracleConnectionFactory {
 
     @Bean
-    public DataSource dataSource() throws SQLException {
+    public HikariDataSource hikariDataSource() throws SQLException {
+        // Configure OracleDataSource
         OracleDataSource oracleDataSource = new OracleDataSource();
         oracleDataSource.setURL("jdbc:oracle:thin:@//localhost:1521/ORCLPDB1");
         oracleDataSource.setUser("your_username");
         oracleDataSource.setPassword("your_password");
 
-        HikariDataSource hikariDataSource = new HikariDataSource();
-        hikariDataSource.setDataSource(oracleDataSource);
-        hikariDataSource.setMaximumPoolSize(10);
+        // Enable Oracle-specific features
+        oracleDataSource.setImplicitCachingEnabled(true);
+        oracleDataSource.setFastConnectionFailoverEnabled(true);
 
-        return hikariDataSource;
+        // Configure HikariCP with OracleDataSource
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDataSource(oracleDataSource);
+        hikariConfig.setMaximumPoolSize(10);
+        hikariConfig.setMinimumIdle(5);
+        hikariConfig.setConnectionTimeout(30000);
+        hikariConfig.setIdleTimeout(600000);
+        hikariConfig.setMaxLifetime(1800000);
+        hikariConfig.setPoolName("HikariPool-Oracle");
+
+        return new HikariDataSource(hikariConfig);
     }
 }
